f05bd5e5b2921410cf7a1174dd1872e06872a6b0
thdgus4025 <thdgus4025@nexlink.co.kr>
2023-06-07 20:24:19 +0900

    flatmap test

---
+++ weather/contants.js
@@ -0,0 +1,45 @@
+//
+const constants = Object.freeze({
+    code: {
+        T1H: {
+            name: '기온',
+            use: true,
+        },
+        RN1: {
+            name: '1시간 강수량',
+            use: false,
+        },
+        SKY: {
+            name: '하늘상태',
+            use: true,
+        },
+        UUU: {
+            name: '동서바람성분',
+            use: false,
+        },
+        VVV: {
+            name: '남북바람성분',
+            use: false,
+        },
+        REH: {
+            name: '습도',
+            use: true,
+        },
+        PTY: {
+            name: '강수형태',
+            use: false,
+        },
+        LGT: {
+            name: '낙뢰',
+            use: false,
+        },
+        VEC: {
+            name: '풍향',
+            use: false,
+        },
+        WSD: {
+            name: '풍속',
+            use: false,
+        },
+    },
+})
